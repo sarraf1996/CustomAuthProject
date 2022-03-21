71f6c137baaec2db91e39e83f7ef4671ffcb1f11
sarraf1996 <sarrafshubham40@gmail.com>
2022-03-22 00:48:57 +0530

    First commit

---
+++ custom_auth_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view()),
+]
---
+++ custom_auth_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+
+def register(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    return render(request, 'register.html', {'form': form})
